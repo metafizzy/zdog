60ee7c89fd867cda32ac36913af16ec8290f3ab8
David DeSandro <desandrocodes@gmail.com>
2018-01-26 22:37:17 -0500

    tall boy

--- demo/4kid-kit/4kid-kit.js
+++ demo/4kid-kit/4kid-kit.js
@@ -4,8 +4,8 @@
 var TAU = Math.PI * 2;
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
-var w = 64;
-var h = 64;
+var w = 72;
+var h = 72;
 var zoom = 6;
 var canvasWidth = canvas.width = w * zoom;
 var canvasHeight = canvas.height = h * zoom;
@@ -15,13 +15,8 @@ var colors = {
   eye: '#444',
   inner: '#FEE',
   cloak: '#F18',
-  // cloak: 'hsla(0, 100%, 50%, 0.2)',
-  armor: '#915',
-  hair: '#521',
-  overalls: '#24D',
-  cloth: '#E11',
-  skin: '#FCA',
-  button: '#FE0',
+  // cloak: 'hsla(0, 100%, 50%, 0.1)',
+  armor: '#926',
 };
 
 var rZSpeed = 0;
@@ -37,35 +32,24 @@ var shapes = [];
 // body center
 new Shape({
   points: [
-    { x: -2, y: 14, z: 0 },
-    { x: -2, y: 16, z: 0 },
-    { x: 2, y: 16, z: 0 },
-    { x: 2, y: 14, z: 0 },
+    { x: -3, y: 10, z: 0 },
+    { x: 0, y: 14, z: 0 },
+    // { x: 2, y: 14, z: 0 },
+    { x: 3, y: 10, z: 0 },
   ],
   color: colors.inner,
-  lineWidth: 12,
+  lineWidth: 13,
 });
 
 // head circle
 new Shape({
   points: [
-    { x: 0, y: -4, z: 0 },
+    { x: 0, y: -12, z: 0 },
   ],
   color: colors.fur,
   lineWidth: 32,
 });
 
-
-// snout
-new Shape({
-  points: [
-    { x: -8, y: 1, z: -4 },
-    { x: 0, y: 1, z: -11 }, // snout tip
-    { x: 8, y: 1, z: -4 },
-  ],
-  color: colors.fur,
-  lineWidth: 14,
-});
 
 [ -1, 1 ].forEach( function( xSide ) {
 
@@ -73,8 +57,8 @@ new Shape({
   var x = 8 * xSide;
   new Shape({
     points: [
-      { x: x, y: -5, z: -11 },
-      { x: x, y: -1, z: -11 },
+      { x: x, y: -13, z: -11 },
+      { x: x, y: -9, z: -11 },
     ],
     color: colors.eye,
     lineWidth: 4,
@@ -82,14 +66,14 @@ new Shape({
 
   // ears
   var frontEarZ = -4;
-  var topEarY = -20;
+  var topEarY = -30;
   var earColor = colors.fur;
 
-  var earA = { x: 14*xSide, y: topEarY+12, z: frontEarZ+4 };
+  var earA = { x: 14*xSide, y: topEarY+12, z: frontEarZ+2 };
   var earB = { x: 14*xSide, y: topEarY, z: frontEarZ };
   var earC = { x: 8*xSide, y: topEarY+11, z: frontEarZ+14 };
   var earD = { x: 10*xSide, y: topEarY, z: frontEarZ };
-  var earE = { x: 3*xSide, y: topEarY+5, z: frontEarZ-1 };
+  var earE = { x: 3*xSide, y: topEarY+5, z: frontEarZ };
   // outer ear
   new Shape({
     points: [ earA, earB, earC ],
@@ -113,8 +97,8 @@ new Shape({
   var innerEarXShift = 4*xSide;
   new Shape({
     points: [
-      { x: earA.x - innerEarXShift , y: earA.y-3, z: frontEarZ+1 },
-      { x: earD.x, y: earD.y+5, z: frontEarZ-1 },
+      { x: earA.x - innerEarXShift , y: earA.y-3, z: frontEarZ },
+      { x: earD.x, y: earD.y+5, z: frontEarZ },
       { x: earE.x + innerEarXShift, y: earE.y+2, z: frontEarZ },
     ],
     color: colors.inner,
@@ -127,8 +111,8 @@ new Shape({
   [ 0, -4 ].forEach( function( yShift ) {
     var whiskerX0 = 10*xSide;
     var whiskerX1 = 16*xSide;
-    var whiskerY0 = 2+yShift;
-    var whiskerY1 = 5+yShift;
+    var whiskerY0 = -5+yShift;
+    var whiskerY1 = -3+yShift;
 
     new Shape({
       points: [
@@ -142,17 +126,26 @@ new Shape({
     });
   });
 
+  // snout
+  new Shape({
+    points: [
+      { x: 6*xSide, y: -5, z: -4 },
+      { x: 0, y: -5, z: -11 }, // snout tip
+    ],
+    color: colors.fur,
+    lineWidth: 14,
+  });
+
 
   // arms
-  var elbow = { x: 12*xSide, y: 12, z: 2.5 };
 
   // shoulder
   new Shape({
     points: [
-      { x: 12*xSide, y: 8, z: 0 },
-      { x: 14*xSide, y: 12, z: 0 },
-      { x: 14*xSide, y: 12, z: 4 },
-      { x: 11*xSide, y: 8, z: 4 },
+      { x: 13*xSide, y: 4, z: 0 },
+      { x: 15*xSide, y: 8, z: 0 },
+      { x: 15*xSide, y: 8, z: 4 },
+      { x: 13*xSide, y: 4, z: 4 },
     ],
     closed: true,
     color: colors.armor,
@@ -161,8 +154,8 @@ new Shape({
   // arm
   new Shape({
     points: [
-      elbow,
-      { x: 12*xSide, y: 16, z: 2 },
+      { x: 12*xSide, y: 12, z: 2.5 },
+      { x: 12*xSide, y: 15, z: 2 },
     ],
     color: colors.fur,
     lineWidth: 8,
@@ -178,7 +171,7 @@ new Shape({
   new Shape({
     points: [
       { x: 6*xSide, y: 20, z: 0 },
-      { x: 6*xSide, y: 24, z: 0 },
+      { x: 6*xSide, y: 26, z: 0 },
     ],
     color: colors.armor,
     lineWidth: 8,
@@ -189,14 +182,14 @@ new Shape({
 var cloakX0 = 8;
 // var cloakX1 = 8;
 
-var cloakY0 = 7;
-var cloakY1 = 9;
+var cloakY0 = 4;
+var cloakY1 = 6;
 var cloakY2 = 13;
-var cloakY3 = 19;
+var cloakY3 = 20;
 
 var cloakZ0 = 0;
-var cloakZ1 = 5;
-var cloakZ2 = 7;
+var cloakZ1 = 6;
+var cloakZ2 = 8;
 
 [
   { ya: cloakY0, za: cloakZ0, yb: cloakY1, zb: cloakZ1 },
@@ -240,7 +233,7 @@ function update() {
   angleZ += rZSpeed;
   // perspective sort
   shapes.sort( function( a, b ) {
-    return ( b.sortValue ) - ( a.sortValue );
+    return b.sortValue - a.sortValue;
   });
   // render shapes
   shapes.forEach( function( shape ) {
