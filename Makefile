1abd1c2772fbc1f5a9a63b89fdf6fc3af671f18e
David DeSandro <desandrocodes@gmail.com>
2019-05-28 22:15:33 -0400

    👟 read bundle file paths in tasks/bundle
    + add make version

--- Makefile
+++ Makefile
@@ -1,9 +1,5 @@
 bundle:
-	cat js/boilerplate.js js/canvas-renderer.js js/svg-renderer.js js/vector.js \
-			js/anchor.js js/dragger.js js/illustration.js js/path-command.js \
-			js/shape.js js/group.js js/rect.js js/rounded-rect.js js/ellipse.js \
-			js/polygon.js js/hemisphere.js js/cylinder.js js/cone.js js/box.js \
-			js/index.js > dist/zdog.dist.js
+	node tasks/bundle
 
 uglify:
 	npx uglifyjs dist/zdog.dist.js -o dist/zdog.dist.min.js --mangle --comments /^!/
@@ -12,3 +8,9 @@ lint:
 	npx jshint js/*.js demos/**/*.js tasks/*.js
 
 dist: bundle uglify
+
+versionTick:
+	node ./tasks/version
+
+version: versionTick dist
+	git add -A dist js
---
+++ tasks/bundle.js
@@ -0,0 +1,19 @@
+/* jshint node: true, esversion: 6, unused: true, undef: true */
+
+const fs = require('fs');
+const execSync = require('child_process').execSync;
+
+// get file paths from index.js
+let indexSrc = fs.readFileSync( './js/index.js', 'utf8' );
+let cjsBlockRegex = /module\.exports = factory\([\w ,'\.\-\(\)\/\n]+;/i;
+let cjsBlockMatch = indexSrc.match( cjsBlockRegex );
+let paths = cjsBlockMatch[0].match( /require\('([\.\-\/\w]+)'\)/gi );
+
+paths = paths.map( function( path ) {
+  return path.replace( `require('.`, 'js' ).replace( `')`, '.js' );
+});
+paths.push('js/index.js');
+
+execSync( `cat ${paths.join(' ')} > dist/zdog.dist.js`);
+
+console.log('bundled dist/zdog.dist.js');
